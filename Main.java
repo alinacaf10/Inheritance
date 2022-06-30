f77072500f0b69f37ed30543c0b2f7cc852ea41f
alinacaf10 <100665352+alinacaf10@users.noreply.github.com>
2022-06-30 17:35:11 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Example.Inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+//        employers employers=new employers("Ali","Nacaf",22,1500);
+//        employers.info();
+        Manager manager=new Manager("Ilham","Aliyev",50,500,"Way",25);
+        manager.info();
+        manager.show();
+    }
+}
